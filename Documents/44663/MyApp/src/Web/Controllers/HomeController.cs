4e8722b6715b30c75ca86f4445aeb626fe4307d7
prakash <mamidisaiprakash@gmail.com>
2019-02-08 14:02:41 -0600

    Added a new view in Views/Home/Privacy1.cshtml

--- Documents/44663/MyApp/src/Web/Controllers/HomeController.cs
+++ Documents/44663/MyApp/src/Web/Controllers/HomeController.cs
@@ -19,6 +19,10 @@ namespace Web.Controllers
         {
             return View();
         }
+         public IActionResult Privacy1()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
